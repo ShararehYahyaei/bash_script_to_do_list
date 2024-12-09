5389cfec083e3bee0bfb530fa6d93dc201044cf4
Sharareh <your.email@example.com>
2024-12-09 22:18:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bash_script_to_do_list
---
+++ todo.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+touch todolist.txt
+
+show_nenu(){
+
+    echo "----TODO APPLICATION------"
+    echo "1.ADD NEW TASK"
+    echo "2.lIST ALL TASKS"
+    echo "3.MARK AS DONE "
+    echo "4.SHOW DONE TASKS"
+    echo "5.SHOW NOT DONE TASKS"
+    echo "6.REMOVE TASK"
+    echo "7.EXIT"  
+}
+
+
+add_task(){
+    read -p "enter yout new task:" task
+    echo "to do task is :$task" >> "todolist.txt"
+    echo "new task added"
+
+}
+
+
+list_tasks(){
+    echo "---all tasks ---"
+
+  if [[ -s "todolist.txt" ]]; then
+
+    nl "todolist.txt"
+  else
+  echo "there is no any tasks"
+ 
+fi
+
+}
+
+mark_done(){
+
+    list_tasks
+    read -p "please enter your task Number :" task_number
+    if  sed -n "$task_number p" "todolist.txt" | grep "to do task is"; then
+    sed -i "$task_number s/to do task is/done/" "todolist.txt"
+    echo "marked as done"
+    else
+    echo "task already done"
+    fi
+}
+ 
+
+ 
+
+
+
+show_all_donetasks(){
+    echo "DONE TASKS"
+    grep "done" "todolist.txt"
+
+}
+show_all_notdonetasks(){
+    echo "ALL TASKS ARE NOT DONE YET"
+    grep "to do task is" "todolist.txt"
+
+}
+
+remove_task(){
+    list_tasks
+
+    read -p "which one do you like to remove id:" remove_number
+  if [[ -n "$remove_number" ]]; then
+  sed -i "${remove_number}d" "todolist.txt"
+  echo "YOUR TASK IS REMOVED"
+ else
+ echo "TASK NOT FOUND"
+ fi
+}
+
+
+
+
+
+ while true; do
+ show_nenu
+ read -p "enter your request:" request
+ if [[ "$request" -eq 1 ]]; then 
+ echo "you chose to add new task"
+ add_task
+
+ elif [[ "$request" -eq 2 ]]; then
+ 
+ list_tasks
+ break
+ elif [[ "$request" -eq 3 ]]; then
+ mark_done
+ break
+
+elif [[ "$request" -eq 4 ]]; then
+show_all_donetasks
+break
+
+ elif [[ "$request" -eq 5 ]]; then
+ show_all_notdonetasks
+ break
+
+
+elif [[ "$request" -eq 6 ]]; then
+remove_task
+break
+elif [[ "$request" -eq 7 ]]; then
+
+exit 0
+
+break
+ else
+ echo "INVALID REQUEST"
+ fi
+ done
